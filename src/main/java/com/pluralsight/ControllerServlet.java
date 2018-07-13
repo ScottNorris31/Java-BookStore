5e6d13d25418a77aea5cb9eadc0a2b048d23fa60
ScottNorris31 <41201957+ScottNorris31@users.noreply.github.com>
2018-07-13 13:44:02 -0400

    Revert "Delete Books"
    This reverts commit 863efad3e326f7860b9bb91dcc280f6c9aed6c94.

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -59,9 +59,6 @@ public class ControllerServlet extends HttpServlet {
 				case "/insert":
 					insertBook(request, response);
           break;
-				case "/delete":
-					deleteBook(request, response);
-          break;
         default:
 				   listBooks(request, response);
            break;
@@ -105,13 +102,6 @@ public class ControllerServlet extends HttpServlet {
 		Book newBook = new Book(title, author, Float.parseFloat(priceString));
 
 		bookDAO.insertBook(newBook);
-		response.sendRedirect("list");
-	}
-	
-	private void deleteBook(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException { 
-		int id = Integer.parseInt(request.getParameter("id"));
-		bookDAO.deleteBook(id);	
 		response.sendRedirect("list");
 	}
 
